f99271636358ae91f8750092acd4029309b7353f
h1ze <burgov@mail.ru>
2022-08-26 19:17:07 +0400

    add 6.1 H.W

--- script.js
+++ script.js
@@ -76,17 +76,69 @@ P.S. Функции вызывать не обязательно*/
 
 5) Добавить нумерацию выведенных фильмов */
 
+/* Задания на урок: 6
+
+1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
+новый фильм добавляется в список. Страница не должна перезагружаться.
+Новый фильм должен добавляться в movieDB.movies.
+Для получения доступа к значению input - обращаемся к нему как input.value;
+P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
+
+2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
+
+3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
+
+4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение: 
+"Добавляем любимый фильм"
+
+5) Фильмы должны быть отсортированы по алфавиту */
+
 "use strict";
 
-const movieDB = {
-  movies: [
-    "Логан",
-    "Лига справедливости",
-    "Ла-ла лэнд",
-    "Одержимость",
-    "Скотт Пилигрим против...",
-  ],
-};
+document.addEventListener("DOMContentLoaded", () => {
+  const movieDB = {
+    movies: [
+      "Логан",
+      "Лига справедливости",
+      "Ла-ла лэнд",
+      "Одержимость",
+      "Скотт Пилигрим против...",
+    ],
+  };
+
+  const addForm = document.querySelector(".add"),
+    addInput = addForm.querySelector(".adding__input"),
+    checkbox = addForm.querySelector("[type='checkbox']");
+
+  addForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const newFilm = addInput.value;
+    const favorite = checkbox.checked;
+
+    movieDB.movies.push(newFilm);
+    movieDB.movies.sort();
+    createMovieList();
+  });
+
+  document.querySelector(".promo__genre").textContent = "Драма";
+  document.querySelector(".promo__adv").remove();
+  document.querySelector(".promo__bg ").style.background =
+    "url(img/bg.jpg) center center/cover no-repeat";
+
+  function createMovieList() {
+    const movieList = document.querySelector(".promo__interactive-list");
+    movieList.innerHTML = "";
+
+    movieDB.movies.forEach((element, index) => {
+      movieList.innerHTML += `<li class="promo__interactive-item">${
+        index + 1
+      }. ${element} 
+    <div class="delete"></div>
+    </li>`;
+    });
+  }
+});
 
 const personalMovieDB = {
   count: 0,
@@ -175,10 +227,6 @@ const personalMovieDB = {
     }
   },
 };
-
-document.querySelector(".promo__genre").textContent = "Драма";
-document.querySelector(".promo__adv").remove();
-document.querySelector(".promo__bg ").style.background = "url(img/bg.jpg) center center/cover no-repeat";
 
 // const a = prompt("Один из последних просмотренных фильмов?", ""),
 //   b = prompt("На сколько оцените его?", ""),
